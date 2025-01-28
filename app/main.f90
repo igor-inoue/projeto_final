b21989609e549e8cce01e2a2fc595245df42f562
igor-inoue <igor.inoue@ufv.br>
2025-01-28 20:56:36 -0300

    calcula as probabilidades da matriz ser +1 para todas as densidades

--- app/main.f90
+++ app/main.f90
@@ -8,28 +8,28 @@ program main
   integer(kind=i4) :: seed = 1!213123
   type(rndgen) :: gerador
 
-  integer(kind=i4) :: arquivo1
+  integer(kind=i4) :: arquivo1, arquivo2
   integer(kind=i4) :: d, N, i, l, c, den, l2, c2, Nmax, m
   integer(kind=i4), allocatable :: rede1d(:), rede2d(:,:), rede3d(:,:,:), dados(:,:)
-  integer(kind=i8) :: t, tsoma
+  integer(kind=i8) :: t, tsoma, soma1
   real(kind=sp) :: r0, r1, ang
-  real(kind=dp) :: tmed
+  real(kind=dp) :: tmed, prob1
   
   d = 2
   Nmax = 12
 
   allocate(rede2d(0:(Nmax-1),0:(Nmax-1)))
 
-  !allocate(dados(0:(Nmax-1)), 0:1)
-
   call gerador%init(seed)
 
   open(newunit=arquivo1, file='dados.dat')
+  open(newunit=arquivo2, file='dados2.dat')
 
   do N = 1, Nmax
     tsoma = 0
 
     do den = 0, N ** 2
+      soma1 = 0
       do i = 1, 20
         rede2d = -1
 
@@ -63,13 +63,16 @@ program main
           rede2d(l,c) = rede2d(l2,c2)
           t = t + 1
         end do
+        if ( rede2d(0,0) .eq. 1 ) then
+          soma1 = soma1 + 1  
+        end if
       end do
       tsoma = tsoma + t
+      prob1 = soma1 / 20.0
+      write(arquivo2,*) N, den, prob1
     end do
 
-    tmed = tsoma / ((N**2 + 1) * 20)
-
-    print*, N, tmed
+    tmed = tsoma / ((N**2 + 1) * 20.0)
 
     write(arquivo1,*) N, tmed
 
