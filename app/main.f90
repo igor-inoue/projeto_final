7eca068d00cef9a2506daae66d1f7c8df2c88d4e
igor-inoue <igor.inoue@ufv.br>
2025-01-28 23:22:08 -0300

    tentativa de análise

--- app/main.f90
+++ app/main.f90
@@ -23,7 +23,7 @@ program main
   call gerador%init(seed)
 
   open(newunit=arquivo1, file='dados.dat')
-  write(arquivo1,*) "N      t_med"
+  write(arquivo1,*) "t_med"
   open(newunit=arquivo2, file='dados2.dat')
 
   do N = 1, Nmax
@@ -75,7 +75,7 @@ program main
 
     tmed = tsoma / ((N**2 + 1) * 20.0)
 
-    write(arquivo1,*) N, tmed
+    write(arquivo1,*) tmed
 
   end do
 
